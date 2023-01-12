bcf628bbed7d36c6e765cdafea7fd5390a7caa34
MiguelRios82 <122577851+MiguelRios82@users.noreply.github.com>
2023-01-12 20:59:42 -0300

    Add files via upload

---
+++ CV/JS/script.js
@@ -0,0 +1,43 @@
+//  Boton 1
+ let boton1 = document.getElementById("boton1");
+
+ let boton_text1 = document.getElementById("boton_text1");
+
+ boton1.addEventListener("click", mostrar_text1);
+
+ function mostrar_text1() {
+  boton_text1.classList.toggle("show");
+ }
+
+//  Boton 2
+ let boton2 = document.getElementById("boton2");
+
+ let boton_text2 = document.getElementById("boton_text2");
+
+ boton2.addEventListener("click", mostrar_text2);
+
+ function mostrar_text2() {
+  boton_text2.classList.toggle("show");
+ }
+
+ //  Boton 3
+ let boton3 = document.getElementById("boton3");
+
+ let boton_text3 = document.getElementById("boton_text3");
+
+ boton3.addEventListener("click", mostrar_text3);
+
+ function mostrar_text3() {
+  boton_text3.classList.toggle("show");
+ }
+
+ //  Boton 
+ let boton4 = document.getElementById("boton4");
+
+ let boton_text4 = document.getElementById("boton_text4");
+
+ boton4.addEventListener("click", mostrar_text4);
+
+ function mostrar_text4() {
+  boton_text4.classList.toggle("show");
+ }
